58b225de19506984ec0e2ab83c60b44168c93380
SHANTO <rabbymd55555@gmail.com>
2025-02-08 22:54:42 +0600

    added index.js file

---
+++ index.js
@@ -0,0 +1,16 @@
+var numbers = [20,11,2,23,67];
+console.log(numbers);
+// get element value by index
+console.log(numbers[3]);
+
+const fourth = numbers[3];
+console.log(fourth);
+
+// set or udate element value by index
+
+numbers[1] = 776;
+console.log(numbers);
+
+numbers[3] = 80;
+console.log(numbers);
+
