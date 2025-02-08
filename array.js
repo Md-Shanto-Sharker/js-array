a55ddc9ae30268693b2fe79a87529075601b2755
SHANTO <rabbymd55555@gmail.com>
2025-02-08 22:39:19 +0600

    added array length file

--- array.js
+++ array.js
@@ -1,3 +1,7 @@
 var numbers = [12, 20, 24, 30, 50];
 var names = ['shanto', 'sharker', 'irfan'];
 var mixedArray = ["shanto",20,'sharker', 30];
+
+console.log(numbers);
+console.log(names);
+console.log(mixedArray);
---
+++ length.js
@@ -0,0 +1,2 @@
+var numbers = [10,2,34,32,45,79];
+console.log(numbers.length);
