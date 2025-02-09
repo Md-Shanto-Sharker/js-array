269f6b4614a415e6342af846a6b4971da82e2c2d
SHANTO <rabbymd55555@gmail.com>
2025-02-09 12:59:11 +0600

    added task-3.js file

--- js-array-task/task-3.js
+++ js-array-task/task-3.js
@@ -2,7 +2,13 @@
 // Instructions:
 
 //             1.Create an array of books containing different book.
-            
 //             2.Use the includes method to check if the array contains a javascript book.
-            
 //             3.Print a message to the console indicating whether the element is present in the array or not.
+
+
+const books = ["python",'java','C++'];
+if(books.includes('javascript')){
+    console.log("This book is include");
+}else{
+    console.log("Nothing");
+}
