344153126249796d17bdd1f84345c26d0bb23304
SHANTO <rabbymd55555@gmail.com>
2025-02-09 13:08:17 +0600

    added task-4.js file

--- js-array-task/task-4.js
+++ js-array-task/task-4.js
@@ -5,3 +5,27 @@
 //             1.Create different variables, each containing either an array or a non-array value.
 //             2.Now use isArray to check if each variable is an array.
 //             3.Print a message to the console indicating whether each variable is an array or no
+
+
+const numbers =[12,45,6,98];
+const friends=[];
+const array ='[]';
+
+if(Array.isArray(numbers)){
+    console.log('This is an array');
+}else{
+    console.log('This is not an array');
+}
+
+
+if(Array.isArray(friends)){
+    console.log("This is an array");
+}else{
+    console.log("This is not an array");
+}
+
+if(Array.isArray(array)){
+    console.log("This is an array");
+}else{
+    console.log("This is not an array");
+}
