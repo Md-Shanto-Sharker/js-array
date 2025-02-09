ecd809c1a680de4b9dec8a8f054135becaffef43
SHANTO <rabbymd55555@gmail.com>
2025-02-09 12:07:25 +0600

    added includes.js file

---
+++ includes.js
@@ -0,0 +1,11 @@
+const name=['shanto','sharker','irfan','rasel'];
+console.log(name.includes('shanto'));
+console.log(name.includes('mujahid'));
+
+
+if(name.includes("shanto")){
+    console.log('party');
+}
+else{
+    console.log('we are fasting');
+}
