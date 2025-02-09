f0129fae1fff9a3846be3fd6a97809d35a396d67
SHANTO <rabbymd55555@gmail.com>
2025-02-09 15:18:39 +0600

    added problem-5.js file

---
+++ extra-practice/problem-5.js
@@ -0,0 +1,13 @@
+// ✅ প্রবলেম 5:
+// দুটি অ্যারে [1, 2, 3] এবং [4, 5, 6] নিয়ে concat() ব্যবহার করে একটি নতুন অ্যারে তৈরি করো।
+
+// তারপর join(", ") ব্যবহার করে এটিকে একটি স্ট্রিংয়ে রূপান্তর করো।
+
+
+
+const first_array=[1, 2, 3] ;
+const second_array=[4, 5, 6] ;
+const new_array =first_array.concat(second_array);
+console.log(new_array.join(", "));
+
+
