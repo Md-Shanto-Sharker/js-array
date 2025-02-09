571cc005586e569c03e22e12cc1d3d16d0bb1463
SHANTO <rabbymd55555@gmail.com>
2025-02-09 12:46:30 +0600

    added task-1.js file

---
+++ js-array-task/task-1.js
@@ -0,0 +1,11 @@
+// 1. Declare an array
+//      1.Declare an array with 5 elements containing fruits
+//      2.console log the 3rd index element
+//      3.change the value of the 2nd index element to jambura
+//      4.console log the final array
+
+const array = ['mango','banana','strawberry','orange','apple'];
+console.log(array);
+console.log(array.indexOf('orange'));
+array[2]='jambura';
+console.log(array);
