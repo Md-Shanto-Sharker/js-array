fd0a6e13c6ba45558b37c5a8e98983c8534d43ed
SHANTO <rabbymd55555@gmail.com>
2025-02-09 13:39:32 +0600

    added task-5.js file

--- js-array-task/task-5.js
+++ js-array-task/task-5.js
@@ -3,3 +3,8 @@
 //             1.Create two arrays of your choice.
 //             2.Use the concat method to combine the two arrays into a new array.
 //             3.Print both the original arrays and the combined array using console.log().
+
+const numbers = [12,43,55,70];
+const name = ['shanto','sharker','irfan'];
+const array =numbers.concat(name);
+console.log(array);
