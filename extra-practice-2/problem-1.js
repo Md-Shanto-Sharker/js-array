dcd96513f1295015b70d49af2d7cd6e65f633f36
SHANTO <rabbymd55555@gmail.com>
2025-02-11 12:01:40 +0600

    added problem-1.js file

---
+++ extra-practice-2/problem-1.js
@@ -0,0 +1,14 @@
+// ১ থেকে ১০ পর্যন্ত সংখ্যা প্রিন্ট করো
+
+// USE of while loop
+let n =1;
+while (n<=10){
+    console.log(n);
+    n++
+}
+
+
+// USE of for loop
+for(let n = 1; n<=10; n++){
+    console.log(n);
+}
