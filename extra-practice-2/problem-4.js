8af160e36575ec6bf24e95aef0c0e583ba6bd0b7
SHANTO <rabbymd55555@gmail.com>
2025-02-11 12:17:53 +0600

    added problem-4.js file

---
+++ extra-practice-2/problem-4.js
@@ -0,0 +1,10 @@
+// নির্দিষ্ট সংখ্যা অ্যারেতে আছে কিনা চেক করো
+
+let numbers = [2, 4, 6, 8, 10];   //let target = 6;
+
+    if(numbers.includes(6)){
+        console.log("Yes");
+    }else{
+        console.log("No");
+    }
+
